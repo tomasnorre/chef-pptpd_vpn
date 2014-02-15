0ce04af944319350121b770ca2608a63188001b3
Tomas Norre <tomas.norre@aoemedia.de>
2014-02-15 11:52:43 +0100

    Fixing errors, from build report

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,38 +5,37 @@
 # Copyright 2014, Tomas Norre
 #
 
-
 # install apache
 package 'pptpd' do
- action :install
+  action :install
 end
 
 # start the apache service
 # make sure the service starts on reboot
 service 'pptpd' do
- action [:start, :enable]
+  action [:start, :enable]
 end
 
 # write user/password file
 cookbook_file '/etc/ppp/chap-secrets' do
- source 'chap-secrets'
- mode '0600'
+  source 'chap-secrets'
+  mode '0600'
 end
 
 # write configuration file
 template 'update pptpd.conf' do
- path '/etc/pptpd.conf'
- source 'pptpd.conf.erb'
- mode 0644
- variables(
-  local_ip: node['pptpd_vpn']['local_ip'],
-  ip_begin: node['pptpd_vpn']['ip_begin'],
-  ip_end_block: node['pptpd_vpn']['ip_end_block']
- )
- notifies :restart, 'service[pptpd]'
+  path '/etc/pptpd.conf'
+  source 'pptpd.conf.erb'
+  mode 0644
+  variables(
+    local_ip: node['pptpd_vpn']['local_ip'],
+    ip_begin: node['pptpd_vpn']['ip_begin'],
+    ip_end_block: node['pptpd_vpn']['ip_end_block']
+  )
+  notifies :restart, 'service[pptpd]'
 end
 
 service 'pptpd' do
- supports :restart => :true
- action [:enable, :start]
+  supports restart: true
+  action [:enable, :start]
 end
